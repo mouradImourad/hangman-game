c57ae3c7b1e12249825657b3bff7723141e63bca
Mourad Mourad <mourad272@icloud.com>
2024-06-28 16:05:48 -0500

    input form created completely

--- src/components/inputform.jsx
+++ src/components/inputform.jsx
@@ -1,17 +1,22 @@
 // i will add some logic to handlke the submit guess button, 
 // create function to handle clicking the button and call it from the button , and test if it working by consol.log 
+// now handleSubmit should update the input  field ***
 // then i need to store those form field values in react component state, let's call it inputField , setInputField
 // we have to import useState from react,
 // then hook this up in the input form 
 // this will lead us to creat another function to handle the input field change and event
-// then hook it up to the input field 
+// then hook it up to the input field using the onChange attribute 
+// then in the handleInputFieldChange i will setInputField to event.target.value
+// then console.log(inputField) to make sure the component state is hooked up right
+// now in fact that input field updating its own react component state that we have created for
+// this button's job is going to be able to send that letter guess somewhere else 
 
 import { useState } from "react";
 function InputForm() {
     const [inputField, setInputField] = useState("")
     const handleSubmit = () => {
         console.log('click') // --> to check if the button work, clicking work and it consol.log 'click' every time you press on the button.
-        setInputField();
+        setInputField();   // ***
     };
     console.log(inputField)
     const handleInputFieldChange = (e) => {
@@ -22,7 +27,7 @@ function InputForm() {
     return (
         <div>
             <h3>input form</h3>
-            <input onChange={handleInputFieldChange} value={inputField}  placeholder="Guess A Letter"></input>
+            <input onChange={handleInputFieldChange} value={inputField}  placeholder="Guess A Letter"></input>  
             <button onClick={(handleSubmit)}>Submit Guess</button>
         </div>
 
